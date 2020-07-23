898d9ae8c388747aaa4648ff758382358a1ad706
Xeaphen <61913614+Xeaphen@users.noreply.github.com>
2020-07-23 16:54:09 +0700

    Added Roda Sprite and an Unfinished Body

--- src/loading.js
+++ src/loading.js
@@ -13,42 +13,50 @@ export class Loading extends Phaser.Scene{
   }
 
   preload(){
-    this.load.image('MENU_BG', './src/assets/MENU_BG.jpg');
+    // this.load.image('MENU_BG', './src/assets/MENU_BG.jpg');
     this.load.image('TAP', './src/assets/TAP_SIGN.png');
-    this.load.image('BM_1P', './src/assets/BM_1P.png');
-    this.load.image('BM_1BPP10', './src/assets/BM_1BPP10.png');
-    this.load.image('BM_1AAD', './src/assets/BM_1AAD.png');
-    this.load.image('BM_2I','./src/assets/BM_2I.png');
-    this.load.image('BM_3TC','./src/assets/BM_3TC.png');
-    this.load.image('BM_4LD','./src/assets/BM_4LD.png');
-    this.load.image('BM_5N','./src/assets/BM_5N.png');
-    this.load.image('BM_5F','./src/assets/BM_5F.png');
-    this.load.image("BM_GEXB",'./src/assets/BM_GEXB.png');
-    this.load.image('BM_CPP','./src/assets/BM_CPP.png');
-    this.load.image('BM_DPP','./src/assets/BM_DPP.png');
-    this.load.image('BM_NEXT','./src/assets/BM_NEXT.png');
-    this.load.image('BM_PREV','./src/assets/BM_PREV.png');
-    this.load.image('PM_1I','./src/assets/PM_1I.png');
-    this.load.image('PM_2TC','./src/assets/PM_2TC.png');
-    this.load.image('PM_3LD','./src/assets/PM_3LD.png');
-    this.load.image('PM_PY','./src/assets/PM_PY.png');
-    this.load.image('LIFE', './src/assets/LIFE.png');
-    this.load.image('DM_PW', './src/assets/DM_PW.png')
-    this.load.image('DM_PP10', './src/assets/DM_PP10.png')
-    this.load.image('DM_ADL', './src/assets/DM_ADL.png')
-    this.load.image('WM_EVW', './src/assets/WM_EVW.png');
-    this.load.image('WM_SE', './src/assets/WM_SE.png');
+    // this.load.image('BM_1P', './src/assets/BM_1P.png');
+    // this.load.image('BM_1BPP10', './src/assets/BM_1BPP10.png');
+    // this.load.image('BM_1AAD', './src/assets/BM_1AAD.png');
+    // this.load.image('BM_2I','./src/assets/BM_2I.png');
+    // this.load.image('BM_3TC','./src/assets/BM_3TC.png');
+    // this.load.image('BM_4LD','./src/assets/BM_4LD.png');
+    // this.load.image('BM_5N','./src/assets/BM_5N.png');
+    // this.load.image('BM_5F','./src/assets/BM_5F.png');
+    // this.load.image("BM_GEXB",'./src/assets/BM_GEXB.png');
+    // this.load.image('BM_CPP','./src/assets/BM_CPP.png');
+    // this.load.image('BM_DPP','./src/assets/BM_DPP.png');
+    // this.load.image('BM_NEXT','./src/assets/BM_NEXT.png');
+    // this.load.image('BM_PREV','./src/assets/BM_PREV.png');
+    // this.load.image('PM_1I','./src/assets/PM_1I.png');
+    // this.load.image('PM_2TC','./src/assets/PM_2TC.png');
+    // this.load.image('PM_3LD','./src/assets/PM_3LD.png');
+    // this.load.image('PM_PY','./src/assets/PM_PY.png');
+    // this.load.image('LIFE', './src/assets/LIFE.png');
+    // this.load.image('DM_PW', './src/assets/DM_PW.png')
+    // this.load.image('DM_PP10', './src/assets/DM_PP10.png')
+    // this.load.image('DM_ADL', './src/assets/DM_ADL.png')
+    // this.load.image('WM_EVW', './src/assets/WM_EVW.png');
+    // this.load.image('WM_SE', './src/assets/WM_SE.png');
 
 //////////////////////////////////////////////////////////////////Atas Dummy Assets bawah real assets
 
     this.load.image('time', './src/assets/time.png');
     this.load.image('timebar', './src/assets/time_bar.png');
-    this.load.image('crate','./src/assets/Crate.png');
+    this.load.image('crate','./src/assets/box.png');
     this.load.image('Roda','./src/assets/roda.png');
     this.load.image('flag','./src/assets/flag.png');
     this.load.image('Exit','./src/assets/Exit.png');
     this.load.image('Leaderboard','./src/assets/Leaderboard_panel.png');
     this.load.image('score','./src/assets/score.png');
+
+    this.load.image('cloud1','./src/assets/cloud.png');
+    this.load.image('cloud2','./src/assets/cloud2.png');
+    this.load.image('cloud3','./src/assets/cloud3.png');
+
+    this.load.image('airplane','./src/assets/pesawat.png');
+    
+    this.load.image('bangunan','./src/assets/building.png');
 
     this.load.image('TestCar','./src/assets/MobilTest.png');
     this.load.json('jsonMobil', './src/assets/Car.json');
@@ -124,7 +132,7 @@ export class Loading extends Phaser.Scene{
 
     this.input.on("pointerdown", () => {
 
-      this.scene.start("MainMenu");
+      this.scene.start("PlayScene");
     })
   }
 
